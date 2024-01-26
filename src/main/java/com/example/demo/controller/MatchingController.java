48ca98df1f0fed524611a071eb8c210c75eaee50
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-01-26 23:45:33 +0900

    2주차 기능구현(json버전)

--- src/main/java/com/example/demo/controller/MatchingController.java
+++ src/main/java/com/example/demo/controller/MatchingController.java
@@ -5,14 +5,12 @@ import com.example.demo.DTO.OptionDTO;
 import com.example.demo.service.MatchingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class MatchingController {
 
@@ -24,26 +22,22 @@ public class MatchingController {
 
     // {user_id}의 키워드 출력
     @GetMapping("/{user_id}/match")
-    public String showOptionsList(@PathVariable String user_id, Model model) {
-        List<OptionDTO> optionsList = matchingService.getOptionsDTOList(user_id);
-        model.addAttribute("optionsList", optionsList);
-        return "getSurvey";
+    public List<OptionDTO> showOptionsList(@PathVariable String user_id) {
+        return matchingService.getOptionsDTOList(user_id);
     }
 
     // {user_id}의 키워드를 가지고 추천 동물 출력
     @PostMapping("/{user_id}/match")
-    public String matchingAnimals(@PathVariable String user_id, Model model) {
+    public ResponseEntity<Object> matchingAnimals(@PathVariable String user_id) {
         List<Integer> optionList = matchingService.getOptionsDTOList(user_id).stream()
                 .map(OptionDTO::getOptionId)
                 .collect(Collectors.toList());
         Set<AnimalDTO> animalList = matchingService.getAnimalDTOList(optionList);
         try {
-            model.addAttribute("animalList", matchingService.sumWeights(optionList, animalList));
-            return "Matching";
+            return new ResponseEntity<>(matchingService.sumWeights(optionList, animalList), HttpStatus.OK);
         }
         catch (NullPointerException e) {
-            model.addAttribute("error", "매칭된 동물이 없습니다.");
-            return "error";
+            return new ResponseEntity<>("매칭된 동물이 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
